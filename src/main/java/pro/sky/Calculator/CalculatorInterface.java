bb6738a97282fb4b8aee72a810433fbac07116c1
AlexDv <DvoinikovAV@yandex.ru>
2024-03-25 20:31:29 +0300

    0

--- src/main/java/pro/sky/Calculator/CalculatorInterface.java
+++ src/main/java/pro/sky/Calculator/CalculatorInterface.java
@@ -2,8 +2,12 @@ package pro.sky.Calculator;
 
 public interface CalculatorInterface {
     String hello();
-    int plus(int num1,int num2);
-    int minus(int num1,int num2);
-    int multiply(int num1,int num2);
-    String divide(String num1,String num2);
+
+    int plus(int num1, int num2);
+
+    int minus(int num1, int num2);
+
+    int multiply(int num1, int num2);
+
+    String divide(String num1, String num2);
 }
