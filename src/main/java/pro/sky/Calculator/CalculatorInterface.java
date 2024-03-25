fe6d346324f0033a79ed63b758a9984c8eb61af2
AlexDv <DvoinikovAV@yandex.ru>
2024-03-25 20:28:33 +0300

    0

---
+++ src/main/java/pro/sky/Calculator/CalculatorInterface.java
@@ -0,0 +1,9 @@
+package pro.sky.Calculator;
+
+public interface CalculatorInterface {
+    String hello();
+    int plus(int num1,int num2);
+    int minus(int num1,int num2);
+    int multiply(int num1,int num2);
+    String divide(String num1,String num2);
+}
